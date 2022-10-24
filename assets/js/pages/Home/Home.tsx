e1230c0b9bc4b1a86a960069521f8096a346bf81
Nguyen Ky Phuong (s117326) <phuong.nguyenky@student.ap.be>
2022-10-24 18:48:19 +0200

    added Header title to home

--- assets/js/pages/Home/Home.tsx
+++ assets/js/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="container">
         <Sidebar/>
         <div className="homeContainer">
-            <Header/>
+            <Header title ="My dashboard"/>
             Content
         </div>
     </div>

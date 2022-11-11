798037eecd0f56bbd2eac6214cfa0b4c23477270
Haraba Jawad (s131255) <jawad.haraba@student.ap.be>
2022-11-11 23:43:21 +0100

    appbar

--- assets/js/components/AppBar/appbar.tsx
+++ assets/js/components/AppBar/appbar.tsx
@@ -1,15 +1,10 @@
-import { Box } from "@mui/material";
-import { GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport, GridToolbarFilterButton, GridToolbarQuickFilter } from "@mui/x-data-grid";
+import { GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport } from "@mui/x-data-grid";
 
 const CustomToolbar = () => {
     return (
-      <GridToolbarContainer style={{justifyContent:"space-between"}}>
-        <Box>
-          <GridToolbarDensitySelector />
-          <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
-          <GridToolbarFilterButton />
-        </Box>
-        <GridToolbarQuickFilter />
+      <GridToolbarContainer>
+        <GridToolbarDensitySelector />
+        <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
       </GridToolbarContainer>
     );
   }
